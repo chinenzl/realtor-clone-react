a1d7c76e93f10948e647054da4f13569c322267b
chinenzl <chinenzl@163.com>
2023-10-04 23:59:17 +1300

    create header and component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from'./pages/Home';
+import Profile from'./pages/Profile';
+import SignIn from'./pages/SignIn';
+import SignUp from'./pages/SignUp';
+import Offers from'./pages/Offers';
+import ForgotPassword from'./pages/ForgotPassword';
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-2xl'>hello</h1>
-    </div>
+   <Router>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      <Route path="/offers" element={<Offers/>}/>
+    </Routes>
+   </Router>
   );
 }
 
